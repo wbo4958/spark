e3b9b334b81fdb76965b31b23bf587c701193840
Bobby Wang <wbo4958@gmail.com>
2024-07-04 17:57:13 +0800

    Support connect ml on spark.ml

---
+++ python/pyspark/ml/remote/readwrite.py
@@ -0,0 +1,55 @@
+from typing import cast, Type
+
+import pyspark.sql.connect.proto as pb2
+from pyspark.ml.remote.serialize import serialize_ml_params, deserialize
+from pyspark.ml.util import MLWriter, MLReader, RL
+from pyspark.sql import SparkSession
+from pyspark.sql.connect.expressions import LiteralExpression
+
+
+class RemoteMLWriter(MLWriter):
+    def __init__(self, instance: "JavaMLWritable") -> None:
+        super().__init__()
+        self._instance = instance
+
+    @property
+    def sc(self, ) -> "SparkContext":
+        raise RuntimeError("Accessing SparkContext is not supported on Connect")
+
+    def save(self, path: str) -> None:
+        from pyspark.ml import Model
+        if isinstance(self._instance, Model):
+            instance = cast("Model", self._instance)
+            id = instance._java_obj
+            session = SparkSession.getActiveSession()
+            params = serialize_ml_params(instance, session)
+
+            writer = pb2.MlCommand.Writer(model_ref=pb2.ModelRef(id=id),
+                                          params=params,
+                                          path=path,
+                                          should_overwrite=self.shouldOverwrite,
+                                          options=self.optionMap)
+            req = session.client._execute_plan_request_with_metadata()
+            req.plan.ml_command.write.CopyFrom(writer)
+            session.client.execute_ml(req)
+
+
+class RemoteMLReader(MLReader):
+    def __init__(self, clazz: Type["JavaMLReadable[RL]"]) -> None:
+        super().__init__()
+        self._clazz = clazz
+
+    def load(self, path: str) -> RL:
+        java_package = (self._clazz.__module__.replace("pyspark", "org.apache.spark") +
+                        "." + self._clazz.__name__)
+        session = SparkSession.getActiveSession()
+        reader = pb2.MlCommand.Reader(clazz=java_package,
+                                      path=path)
+        req = session.client._execute_plan_request_with_metadata()
+        req.plan.ml_command.read.CopyFrom(reader)
+        model_info = deserialize(session.client.execute_ml(req))
+        instance = self._clazz(model_info.model_ref.id)
+        instance._resetUid(model_info.uid)
+        params = {k: LiteralExpression._to_value(v) for k, v in model_info.params.params.items()}
+        instance._set(**params)
+        return instance
